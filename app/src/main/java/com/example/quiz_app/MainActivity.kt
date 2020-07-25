4a6c39982de518fa759690b33b36270c5e2f8fdb
JUN_BRO <qkrwns708@soongsil.ac.kr>
2020-07-25 19:14:21 +0900

    2020 / 07 / 20 modified

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.quiz_app
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -24,47 +24,35 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.q_07, false),
         Question(R.string.q_08, true),
         Question(R.string.q_09, true),
-        Question(R.string.q_10, false))
-
-
-    private val text = hashMapOf(1 to "글로벌미디어학부는 IT대학이다.",
-        2 to "숭실대학교는 상도역에 있다.",
-        3 to "준형이는 24살이다.",
-        4 to "종헌이형은 올해 졸업작품을 한다.",
-        5 to "준형이는 크게 될 인물이다.",
-        6 to "영준이형은 28살이다.",
-        7 to "종헌이형은 시원스쿨을 한다.",
-        8 to "롤을 제일 잘하는 사람은 준형이다.",
-        9 to "우리는 언타이틀이다.",
-        10 to "안드로이드 스터디는 재미없다."
+        Question(R.string.q_10, false),
+        Question(R.string.q_10, false)
     )
 
-    val text_a = listOf(2, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0)
-    var text_c = mutableListOf<Int>()
+
+    var q_answer = mutableListOf<Boolean>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
     }
 
     fun onButtonOClicked(v: View) {
-        quiz!!.text = text[num]
-        text_c.add(1)
+            quiz.setText(question[cost_num].quizNum)
+            q_answer.add(true)
 
-        when {
-            num == 1 -> {
-                Toast.makeText(this, "문제 시작!", Toast.LENGTH_SHORT).show()
+            when {
+                num == 1 -> {
+                    Toast.makeText(this, "문제 시작!", Toast.LENGTH_SHORT).show()
+                }
+                question[num - 2].answer == q_answer[num - 1] -> {
+                    Toast.makeText(this, "정답입니다.", Toast.LENGTH_SHORT).show()
+                    score += 10
+                }
+                else -> {
+                    Toast.makeText(this, "오답입니다.", Toast.LENGTH_SHORT).show()
+                }
             }
-            text_a[num-1] == text_c[num-1] -> {
-                Toast.makeText(this, "정답입니다.", Toast.LENGTH_SHORT).show()
-                score += 10
-            }
-            else -> {
-                Toast.makeText(this, "오답입니다.", Toast.LENGTH_SHORT).show()
-            }
-        }
 
         num++
 
@@ -92,21 +80,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onButtonXClicked(v: View) {
-        quiz!!.text = text[num]
-        text_c.add(0)
+            quiz.setText(question[cost_num].quizNum)
+            q_answer.add(false)
 
-        when {
-            num == 1 -> {
-                Toast.makeText(this, "문제 시작!", Toast.LENGTH_SHORT).show()
+            when {
+                num == 1 -> {
+                    Toast.makeText(this, "문제 시작!", Toast.LENGTH_SHORT).show()
+                }
+                question[num - 2].answer == q_answer[num - 1] -> {
+                    Toast.makeText(this, "정답입니다.", Toast.LENGTH_SHORT).show()
+                    score += 10
+                }
+                else -> {
+                    Toast.makeText(this, "오답입니다.", Toast.LENGTH_SHORT).show()
+                }
             }
-            text_a[num-1] == text_c[num-1] -> {
-                Toast.makeText(this, "정답입니다.", Toast.LENGTH_SHORT).show()
-                score += 10
-            }
-            else -> {
-                Toast.makeText(this, "오답입니다.", Toast.LENGTH_SHORT).show()
-            }
-        }
 
         num++
 
