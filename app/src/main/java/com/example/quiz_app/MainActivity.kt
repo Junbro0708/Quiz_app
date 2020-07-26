d7e3c48bd670fb3b617eb9ff0c4ef8f926c2b947
JUN_BRO <qkrwns708@soongsil.ac.kr>
2020-07-26 16:40:38 +0900

    2020 / 07 / 20 modified

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -8,7 +8,7 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var num: Int = 1 // 문제
+    var num: Int = 0 // 문제
     var score: Int = 0 // 점수
 
     val question = arrayOf(
@@ -21,10 +21,6 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.q_07, false),
         Question(R.string.q_08, true),
         Question(R.string.q_09, true),
-        Question(R.string.q_10, false),
-        Question(R.string.q_10, false),
-        Question(R.string.q_10, false),
-        Question(R.string.q_10, false),
         Question(R.string.q_10, false)
     )
 
@@ -37,83 +33,72 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onButtonOClicked(v: View) {
-        if(num < 11) {
-            quiz.setText(question[num - 1].quizNum)
+        if (num < 10) {
+            quiz.setText(question[num].quizNum)
             q_answer.add(true)
 
-            when {
-                num == 1 -> {
-                    Toast.makeText(this, "문제 시작!", Toast.LENGTH_SHORT).show()
-                }
-                question[num - 2].answer == q_answer[num - 1] -> {
-                    Toast.makeText(this, "정답입니다.", Toast.LENGTH_SHORT).show()
-                    score += 10
-                }
-                else -> {
-                    Toast.makeText(this, "오답입니다.", Toast.LENGTH_SHORT).show()
-                }
+            if(num == 0) {
+                Toast.makeText(this, "문제 시작!", Toast.LENGTH_SHORT).show()
+            }else {
+                grading(num)
             }
 
-            cost!!.text = "$num / 10"
+            cost!!.text = "${num + 1} / 10"
             num++
 
-        }else if(num == 11) {
-            when (score) {
-                in 0..20 -> quiz.setTextColor(Color.RED)
-                in 21..50 -> quiz.setTextColor(Color.argb(255, 255, 127, 0))
-                in 51..80 -> quiz.setTextColor(Color.YELLOW)
-                in 81..100 -> quiz.setTextColor(Color.BLACK)
-            }
-            quiz.textSize += 10
-            cost!!.text = " 문제풀기 성공!! "
-            quiz!!.text = score.toString() + "점"
-
+        } else if (num == 10) {
+            grade(score)
             num++
-        }else {
+        } else {
             Toast.makeText(this, "문제를 다 풀었습니다.", Toast.LENGTH_SHORT).show()
             cost!!.text = " 문제풀기 성공!! "
         }
     }
-
 
 
     fun onButtonXClicked(v: View) {
-        if(num < 11) {
-            quiz.setText(question[num - 1].quizNum)
+        if (num < 10) {
+            quiz.setText(question[num].quizNum)
             q_answer.add(false)
 
-            when {
-                num == 1 -> {
-                    Toast.makeText(this, "문제 시작!", Toast.LENGTH_SHORT).show()
-                }
-                question[num - 2].answer == q_answer[num - 1] -> {
-                    Toast.makeText(this, "정답입니다.", Toast.LENGTH_SHORT).show()
-                    score += 10
-                }
-                else -> {
-                    Toast.makeText(this, "오답입니다.", Toast.LENGTH_SHORT).show()
-                }
+            if(num == 0) {
+                Toast.makeText(this, "문제 시작!", Toast.LENGTH_SHORT).show()
+            }else {
+                grading(num)
             }
 
-            cost!!.text = "$num / 10"
+            cost!!.text = "${num + 1} / 10"
             num++
 
-        }else if(num == 11) {
-            when (score) {
-                in 0..20 -> quiz.setTextColor(Color.RED)
-                in 21..50 -> quiz.setTextColor(Color.argb(255, 255, 127, 0))
-                in 51..80 -> quiz.setTextColor(Color.YELLOW)
-                in 81..100 -> quiz.setTextColor(Color.BLACK)
-            }
-            quiz.textSize += 10
-            cost!!.text = " 문제풀기 성공!! "
-            quiz!!.text = score.toString() + "점"
-
+        } else if (num == 10) {
+            grade(score)
             num++
-        }else {
+        } else {
             Toast.makeText(this, "문제를 다 풀었습니다.", Toast.LENGTH_SHORT).show()
             cost!!.text = " 문제풀기 성공!! "
         }
+    }
+
+    fun grade(score: Int) {
+        when (score) {
+            in 0..20 -> quiz.setTextColor(Color.RED)
+            in 21..50 -> quiz.setTextColor(Color.argb(255, 255, 127, 0))
+            in 51..80 -> quiz.setTextColor(Color.YELLOW)
+            in 81..100 -> quiz.setTextColor(Color.BLACK)
+        }
+        quiz.textSize += 10
+        cost!!.text = " 문제풀기 성공!! "
+        quiz!!.text = score.toString() + "점"
+    }
+
+    fun grading(num: Int) {
+        if(question[num - 1].answer == q_answer[num]){
+                Toast.makeText(this, "정답입니다.", Toast.LENGTH_SHORT).show()
+                score += 10
+        }else{
+            Toast.makeText(this, "오답입니다.", Toast.LENGTH_SHORT).show()
         }
     }
+}
+
 
